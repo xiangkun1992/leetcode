af68bdf733d6d55b30ee19e2595af16b58065881
kun.xiang <545659823@qq.com>
2020-12-09 10:42:18 +0800

    2020-12-09 test2

--- src/main/java/trying/TestOne.java
+++ src/main/java/trying/TestOne.java
@@ -4,4 +4,13 @@ package trying;
  * @author kun.xiang
  */
 public class TestOne {
+    private String test1;
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public void setTest1(String test1) {
+        this.test1 = test1;
+    }
 }
