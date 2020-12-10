1f03ac9417e47c85e0facdf3b1b30f1f2cd83d2d
kun.xiang <545659823@qq.com>
2020-12-10 20:01:06 +0800

    2020-12-10 leetCode-> 9:回文数 优化

--- src/main/java/leetcode/Solution9.java
+++ src/main/java/leetcode/Solution9.java
@@ -6,7 +6,7 @@ package leetcode;
 public class Solution9 {
 
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
         return x == cover(x);
